5081c151007150a9799a5fc525b2714bbffda10b
221250009 <221250009@smail.nju.edu.cn>
2023-11-04 20:33:19 +0800

    commit Percolation and PercolationStats

---
+++ percolation/Percolation.java
@@ -0,0 +1,95 @@
+/* *****************************************************************************
+ *  Name:              Alan Turing
+ *  Coursera User ID:  123456
+ *  Last modified:     1/1/2019
+ **************************************************************************** */
+
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class Percolation {
+
+    private boolean[][] sites;
+    private int sizeOfGrid;
+    private int countOfOpenSites;
+    private WeightedQuickUnionUF ufArray;
+
+    // creates n-by-n grid, with all sites initially blocked
+    public Percolation(int n) {
+        if (n <= 0) {
+            throw new IllegalArgumentException("size of grid " + n + " should be positive.");
+        }
+        sites = new boolean[n + 2][n + 2];
+        sizeOfGrid = n;
+        countOfOpenSites = 0;
+        ufArray = new WeightedQuickUnionUF(n * n + 1);
+    }
+
+    // opens the site (row, col) if it is not open already
+    public void open(int row, int col) {
+        validate(row, col);
+        if (isOpen(row, col))
+            return;
+        sites[row][col] = true;
+        countOfOpenSites += 1;
+        // 若此 site 在第一排，将其与 ufArray[0] 连通
+        if (row == 1)
+            connectTop(row, col);
+        // 若相邻的 site isOpen，将它们连通
+        connectNeighbour(row, col);
+    }
+
+    private void connectNeighbour(int row, int col) {
+        if (sites[row - 1][col])
+            ufArray.union(getUFArrayIndex(row, col), getUFArrayIndex(row - 1, col));
+        if (sites[row + 1][col])
+            ufArray.union(getUFArrayIndex(row, col), getUFArrayIndex(row + 1, col));
+        if (sites[row][col - 1])
+            ufArray.union(getUFArrayIndex(row, col), getUFArrayIndex(row, col - 1));
+        if (sites[row][col + 1])
+            ufArray.union(getUFArrayIndex(row, col), getUFArrayIndex(row, col + 1));
+    }
+
+    private void connectTop(int row, int col) {
+        ufArray.union(getUFArrayIndex(row, col), 0);
+    }
+
+    // 获取二维 grid 中的 site 在一维 ufArray 中对应的 index
+    private int getUFArrayIndex(int row, int col) {
+        return sizeOfGrid * (row - 1) + col;
+    }
+
+    // 判断合法坐标
+    private void validate(int row, int col) {
+        if (row <= 0 || row > sizeOfGrid || col <= 0 || col > sizeOfGrid) {
+            throw new IllegalArgumentException(
+                    "index (" + row + ", " + col + ") is not between 0 and " + (sizeOfGrid - 1));
+        }
+    }
+
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col) {
+        validate(row, col);
+        return sites[row][col];
+    }
+
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col) {
+        validate(row, col);
+        return ufArray.find(getUFArrayIndex(row, col)) == ufArray.find(0);
+    }
+
+    // returns the number of open sites
+    public int numberOfOpenSites() {
+        return countOfOpenSites;
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+        for (int i = 1; i <= sizeOfGrid; ++i) {
+            if (isFull(sizeOfGrid, i)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ percolation/PercolationStats.java
@@ -0,0 +1,69 @@
+/* *****************************************************************************
+ *  Name:              Alan Turing
+ *  Coursera User ID:  123456
+ *  Last modified:     1/1/2019
+ **************************************************************************** */
+
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
+public class PercolationStats {
+
+    private double[] fractionOfOpenSites;
+    private int sizeOfGrid;
+    private int numberOfTrials;
+
+    // perform independent trials on an n-by-n grid
+    public PercolationStats(int n, int trials) {
+        if (n <= 0 || trials <= 0) {
+            throw new IllegalArgumentException();
+        }
+        sizeOfGrid = n;
+        numberOfTrials = trials;
+        fractionOfOpenSites = new double[numberOfTrials];
+        for (int i = 0; i < trials; ++i) {
+            fractionOfOpenSites[i] = experiment();
+        }
+    }
+
+    private double experiment() {
+        Percolation p = new Percolation(sizeOfGrid);
+        while (!p.percolates()) {
+            int row = StdRandom.uniformInt(1, sizeOfGrid + 1);
+            int col = StdRandom.uniformInt(1, sizeOfGrid + 1);
+            p.open(row, col);
+        }
+        return p.numberOfOpenSites() * 1.0 / (sizeOfGrid * sizeOfGrid);
+    }
+
+    // sample mean of percolation threshold
+    public double mean() {
+        return StdStats.mean(fractionOfOpenSites);
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev() {
+        return numberOfTrials == 1 ? Double.NaN : StdStats.stddev(fractionOfOpenSites);
+    }
+
+    // low endpoint of 95% confidence interval
+    public double confidenceLo() {
+        return mean() - 1.96 * stddev() / Math.sqrt(numberOfTrials);
+    }
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHi() {
+        return mean() + 1.96 * stddev() / Math.sqrt(numberOfTrials);
+    }
+
+    // test client (see below)
+    public static void main(String[] args) {
+        PercolationStats p = new PercolationStats(Integer.parseInt(args[0]),
+                                                  Integer.parseInt(args[1]));
+        StdOut.println("mean = " + p.mean());
+        StdOut.println("stddev = " + p.stddev());
+        StdOut.println(
+                "95% confidence interval = [" + p.confidenceLo() + ", " + p.confidenceHi() + "]");
+    }
+}
